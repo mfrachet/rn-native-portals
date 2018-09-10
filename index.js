753f75db045172239c537d71a435cdd787b65570
mfrachet <marvin.frachet@zenika.com>
2018-09-10 08:16:05 +0200

    chore(init): remove all exisiting stuff to create a safe lib

--- index.js
+++
@@ -1,4 +0,0 @@
-import ReparentableDestination from './src/ReparentableDestination';
-import ReparentableOrigin from './src/ReparentableOrigin';
-
-export default { ReparentableDestination, ReparentableOrigin };
--- ios/rnReparentable.swift
+++
@@ -1,9 +0,0 @@
-//
-//  rnReparentable.swift
-//  rnReparentable
-//
-//  Created by PC on 23/02/2018.
-//  Copyright © 2018 Facebook. All rights reserved.
-//
-
-import Foundation
