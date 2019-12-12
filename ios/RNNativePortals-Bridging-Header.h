1b77e0a93633f6ef26f355e4aa3a95f196b581a3
alex4dev <corlan198@gmail.com>
2019-12-12 14:12:25 +0100

    ios: add missing bridging header, add missing class references, modify swift version 4

---
+++ ios/RNNativePortals-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+// required for all types of bridge
+#import "React/RCTBridgeModule.h"
+
+// required only for UI Views
+#import "React/RCTViewManager.h"
